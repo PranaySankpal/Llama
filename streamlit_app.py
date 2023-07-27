9441fe5715548a428341fe6f42e127708127bdf8
PranaySankpal <99657714+PranaySankpal@users.noreply.github.com>
2023-07-27 16:04:35 +0530

    Updated title 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot Capgemini POC")
+st.set_page_config(page_title="🦙💬 Capgemini POC")
 
 # Replicate Credentials
 with st.sidebar:
